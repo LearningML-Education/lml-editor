d666744ef232453cc2ce8aa22e5759a1ae37ecaf
Juan David <juanda.rodriguez@educacion.gob.es>
2023-12-04 20:06:32 +0100

    se añade el botón about y la ventana modal de agradecimientos

--- src/main-menu/about-menu.js
+++ src/main-menu/about-menu.js
@@ -8,13 +8,23 @@ export class AboutMenu extends LitElement {
 
   _configConsumer = new ContextConsumer(this, { context: configContext });
 
+  static properties = {
+    activeClass: { String }
+  }
+
   constructor() {
     super();
+    this.showAbout = false;
+    this.activeClass = "";
     updateWhenLocaleChanges(this);
   }
 
   handleClick() {
-   return;
+    this.activeClass = "is-active";
+  }
+
+  handleHide() {
+    this.activeClass = "";
   }
 
   render() {
@@ -25,6 +35,40 @@ export class AboutMenu extends LitElement {
         ${msg("About")}
       </button>
     </div>
+    <div class="modal ${this.activeClass}">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+        <div class="card">
+            <div class="card-image">
+                <figure class="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-left">
+                        <figure class="image is-48x48">
+                            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-4">John Smith</p>
+                        <p class="subtitle is-6">@johnsmith</p>
+                    </div>
+                </div>
+
+                <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button @click=${this.handleHide} class="modal-close is-large" aria-label="close"></button>
+</div> 
     `
   }
 
