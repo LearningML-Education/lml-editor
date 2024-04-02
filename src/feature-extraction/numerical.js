62bcbaa5f262a6263f673329a61a83da738ce0a5
Juan David <juanda.rodriguez@educacion.gob.es>
2024-04-02 15:34:47 +0200

    refactorizando encoder para que sea más simple construir nuevos encoders y añadirlos 3

--- src/feature-extraction/numerical.js
+++ src/feature-extraction/numerical.js
@@ -1,13 +1,12 @@
 import * as tf from '@tensorflow/tfjs';
 
 export function numericalEncoder() {
-    return Promise.resolve({
-        embed: (items) => new Promise((resolve, reject) => {
-            let features = [];
-            for (let csv of items) {
-                features.push(csv.split(",").map(v => parseFloat(v)));
-            }
-            resolve(tf.stack(features));
-        })
+    return  (items) => new Promise((resolve, reject) => {
+        let features = [];
+        for (let csv of items) {
+            features.push(csv.split(",").map(v => parseFloat(v)));
+        }
+        resolve(tf.stack(features));
     });
+    
 } 
