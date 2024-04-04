fe9dc7b8889655ed3e94b01fbba8247e12553453
Juan David <juanda.rodriguez@educacion.gob.es>
2024-04-04 13:30:56 +0200

    consigo hacer que plotly pinte en shadowdom usando firstUpdated y querySelector

--- src/components/model-editor/model-learn.js
+++ src/components/model-editor/model-learn.js
@@ -10,6 +10,7 @@ import {
   modelContext
 } from '../../contexts.js';
 import { classMap } from 'lit/directives/class-map.js';
+import Plotly from 'plotly.js-dist-min'
 
 export class ModelLearn extends LitElement {
 
@@ -24,6 +25,46 @@ export class ModelLearn extends LitElement {
     buttonLoading: { type: Boolean },
     algorithm: { type: String },
     advancedMode: { type: Object, attribute: 'advanced-mode' }
+  }
+
+  firstUpdated() {
+
+    let gd = this.querySelector("#gd");
+  
+    
+    var trace1 = {
+      x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      y: [20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17],
+      type: 'bar',
+      name: 'Primary Product',
+      marker: {
+        color: 'rgb(49,130,189)',
+        opacity: 0.7,
+      }
+    };
+
+    var trace2 = {
+      x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      y: [19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16],
+      type: 'bar',
+      name: 'Secondary Product',
+      marker: {
+        color: 'rgb(204,204,204)',
+        opacity: 0.5
+      }
+    };
+
+    var data = [trace1, trace2];
+
+    var layout = {
+      title: '2013 Sales Report',
+      xaxis: {
+        tickangle: -45
+      },
+      barmode: 'group',
+    };
+
+    Plotly.newPlot(gd, data, layout);
   }
 
   constructor() {
@@ -163,6 +204,10 @@ export class ModelLearn extends LitElement {
 
   render() {
     return html`
+
+<div id="gd"></div>
+
+
     <div class=${classMap({ "modal": true, "is-active": this.buttonLoading })}>
       <div class="modal-background"></div>
       <div class="modal-content">
