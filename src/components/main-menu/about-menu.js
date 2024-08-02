746ea794535344d0df20bc156f7f91f0829c86bd
Juan David <juanda.rodriguez@educacion.gob.es>
2024-08-02 14:06:36 +0200

    Actualiza `about-menu.js` y reemplaza imagen en `genio_maquina.jpg`
    - Cambia la imagen `genio_maquina.jpg` en `public/images`.
    - Modifica la etiqueta `figure` añadiendo la clase `figure` y cambia el atributo `alt` a "LearningML Genius".
    - Añade un nuevo bloque `media-content` para incluir información sobre traducciones e imágenes:
      - Traducciones realizadas con chatGPT (sujetas a revisión).
      - Imágenes de portada generadas con DALL-E.

--- src/components/main-menu/about-menu.js
+++ src/components/main-menu/about-menu.js
@@ -35,18 +35,23 @@ export class AboutMenu extends LitElement {
     <div class="modal-content">
         <div class="card">
             <div class="card-image">
-                <figure">
-                    <img src="${process.env.URL_BASE}/images/genio_maquina.jpg" alt="Placeholder image">
+                <figure class="figure">
+                    <img src="${process.env.URL_BASE}/images/genio_maquina.jpg" alt="LearningML Genius">
                 </figure>
             </div>
             <div class="card-content">
-                <div class="media">                   
+                <div class="media">                                    
                     <div class="media-content">
                         <p>LearningML® Copyright ©2024 Juan David Rodríguez García</p>
                         <p class="is-size-7">Todos los derechos reservados. "LearningML®" es una marca registrada de Juan David Rodríguez García.</p>
                     </div>
                 </div>
-
+                <div class="media">
+                  <div class="media-content">
+                        <p>Traducciones realizadas con chatGPT (sujetas a revisión).</p>
+                        <p>Imágenes de portada generadas con DALL-E.</p>
+                    </div>   
+                  </div>
                 <div class="content">
                   <p>Aprende Machine Learning de la manera más fácil y divertida</p>
                   <a target="_blank" href="https://web.learningml.org/agradecimientos/">Agradecimientos</a> |
