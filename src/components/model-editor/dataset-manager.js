f0124d24ce3729921e88830b8061a9c008d68c5f
Juan David <juanda.rodriguez@educacion.gob.es>
2024-09-22 17:57:20 +0200

    - Corrección de espaciado en las clases CSS. - Añadidas nuevas clases CSS (`audio-container`, `sample-text`, `play-button`, `delete-button`) para mejorar la estructura de los contenedores de audio y botones. - Refactorización de la creación de elementos HTML en `dataset-manager.js` para utilizar las nuevas clases CSS, simplificando el estilo inline.

--- src/components/model-editor/dataset-manager.js
+++ src/components/model-editor/dataset-manager.js
@@ -283,7 +283,7 @@ export class DatasetManager extends LitElement {
         const reader = new FileReader();
         reader.readAsDataURL(audioBlob);
         reader.onloadend = () => {
-          const base64Audio = reader.result.split(',')[1]; // Parte de la data en Base64                               
+          const base64Audio = reader.result.split(',')[1];
 
           if (this._datasetConsumer.value.get(this.labelName)) {
             this._datasetConsumer.value.get(this.labelName).add(base64Audio);
@@ -291,68 +291,39 @@ export class DatasetManager extends LitElement {
 
           const audioUrl = URL.createObjectURL(audioBlob);
 
-          // Crear un contenedor para la imagen y los botones                                                          
           const audioContainer = document.createElement('div');
-          audioContainer.style.position = 'relative';
-          audioContainer.style.display = 'inline-block';
+          audioContainer.className = 'audio-container';
 
-          // Crear la imagen                                                                                           
           const img = document.createElement('img');
           img.src = 'public/images/sound.png';
           img.className = 'sound-image';
 
-          // Crear el texto de muestra                                                                                 
           const sampleText = document.createElement('div');
-          sampleText.textContent = `sample-${this._datasetConsumer.value.get(this.labelName).size + this.deletedSounds}`;
-          sampleText.style.position = 'absolute';
-          sampleText.style.top = '5px';
-          sampleText.style.left = '5px';
-          sampleText.style.color = 'white';
-          sampleText.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-          sampleText.style.padding = '2px 5px';
-          sampleText.style.borderRadius = '3px';
-          sampleText.style.fontSize = '12px';
+          sampleText.textContent = `sample-${this._datasetConsumer.value.get(this.labelName).size + this.deletedSounds}`
+          sampleText.className = 'sample-text';
 
-          // Crear el botón de reproducir                                                                              
           const playButton = document.createElement('button');
-          playButton.className = 'button is-primary';
+          playButton.className = 'button is-primary play-button';
           playButton.innerHTML = `<i class="fa-solid fa-play"></i>`;
-          playButton.style.position = 'absolute';
-          playButton.style.bottom = '10px';
-          playButton.style.left = '10px';
-          playButton.style.fontSize = '12px';
-          playButton.style.padding = '5px';
-          playButton.style.width = '30px';
-          playButton.style.height = '30px';
           playButton.addEventListener('click', () => {
             const audio = new Audio(audioUrl);
             audio.play();
           });
 
-          // Crear el botón de borrar                                                                                  
           const deleteButton = document.createElement('button');
-          deleteButton.className = 'button is-danger';
+          deleteButton.className = 'button is-danger delete-button';
           deleteButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-          deleteButton.style.position = 'absolute';
-          deleteButton.style.bottom = '10px';
-          deleteButton.style.right = '10px';
-          deleteButton.style.fontSize = '12px';
-          deleteButton.style.padding = '5px';
-          deleteButton.style.width = '30px';
-          deleteButton.style.height = '30px';
           deleteButton.addEventListener('click', () => {
             sonidosDiv.removeChild(audioContainer);
             this._datasetConsumer.value.get(this.labelName).delete(base64Audio);
             this.deletedSounds += 1;
           });
 
-          // Añadir la imagen, el texto y los botones al contenedor                                                    
           audioContainer.appendChild(img);
           audioContainer.appendChild(sampleText);
           audioContainer.appendChild(playButton);
           audioContainer.appendChild(deleteButton);
 
-          // Añadir el contenedor al div de sonidos                                                                    
           sonidosDiv.appendChild(audioContainer);
           sonidosDiv.appendChild(document.createElement('br'));
 
